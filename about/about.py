cbabf4be70f8a3ade3010f2e054a8b8b843d6b82
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-11 14:55:02 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -12,7 +12,13 @@ class Mycog:
         """This does stuff!"""
 
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+embed=discord.Embed(title="basic test", url='http://www.mydomain.com', description="This is a description", color=0x207cee)
+embed.set_author(name="UnseenMagik", url='http://www.discordlink.com', icon_url='http://imageicon.com')
+embed.set_thumbnail(url='http://www.imageurl.com')
+embed.add_field(name="Test Field 1", value="Result", inline=True)
+embed.add_field(name="Test Field 2", value="Inline text", inline=True)
+embed.set_footer(text="Footer notes here. ")
+await self.bot.say(embed=embed)```
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
