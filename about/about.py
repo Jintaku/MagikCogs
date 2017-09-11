176da338c2dc98b85f713efd8a36e6db006e92a2
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-11 17:17:04 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -1,48 +1,18 @@
-from random import choice, randint
-from discord.ext import commands
 import discord
-from cogs.utils import checks
+from discord.ext import commands
 
 class AboutMagikBot:
-    """Make announcements in embeds"""
+    """This is information about Magik Bot"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, no_pm=True, aliases=['ab'])
-    @checks.admin_or_permissions(administrator=True)
-    async def about(self, ctx, text: str, color: str = '000000'):
-        """Send a embed about notification"""
+    @commands.command()
+    async def about(self):
+        """This does stuff!"""
 
-const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-
-  message.channel.send({embed});
+        #Your code will go here
+        await self.bot.say("Embed ?about coming soon!")
 
 def setup(bot):
-    bot.add_cog(AboutMagikBot(bot))
+    bot.add_cog(Mycog(bot))
