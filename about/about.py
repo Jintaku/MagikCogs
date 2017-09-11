980e7019ddcad58709ea8e1446aaff3829e61086
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-11 15:45:07 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -1,30 +1,41 @@
 import discord
 from discord.ext import commands
 
+class Mycog:
+    """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
-        
-        class discord.Embed(**kwargs)
-    """My custom cog that does stuff!"""
 
-@commands.command(pass_context=True)
-    async def about(self, ctx):
-        """CTX example command"""
-embed = discord.Embed(title="title ~~(did you know you can have markdown here too?)~~", colour=discord.Colour(0xa6550d), url="https://discordapp.com", description="this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```", timestamp=datetime.datetime.utcfromtimestamp(1505140699))
+const embed = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
-embed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
-embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
-embed.set_author(name="author name", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
-embed.set_footer(text="footer text", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
-
-embed.add_field(name="🤔", value="some of these properties have certain limits...")
-embed.add_field(name="😱", value="try exceeding some of them!")
-embed.add_field(name="🙄", value="an informative error should show up, and this view will remain as-is until all issues are fixed")
-embed.add_field(name="<:thonkang:219069250692841473>", value="these last two", inline=True)
-embed.add_field(name="<:thonkang:219069250692841473>", value="are inline fields", inline=True)
-
-await bot.say(content="this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", embed=embed)
+  message.channel.send({embed});
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
