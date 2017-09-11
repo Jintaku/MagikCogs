1dc540b7b3cf935f710ee55faa032b613952e917
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-11 15:20:25 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -6,18 +6,13 @@ class Mycog:
 
     def __init__(self, bot):
         self.bot = bot
-        
-@client.event
-async def on_message(message):
-    if message.content.startswith('about'):
-        embed = discord.Embed(colour=0xFF0000, description=description)  # Can use discord.Colour()
-        embed.title = "Cool title for my embed"
-        embed.set_author(name=str(author.name), icon_url=author.avatar_url)
-        embed.add_field(name=field_name, value=field_contents)  # Can add multiple fields.
-        embed.set_footer(text=footer_text)
 
-        await self.bot.say(embed=embed)
+    @commands.command()
+    async def mycom(self):
+        """This does stuff!"""
 
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
