d9d29147723a24d570824d02ee91c6b0f6f15d80
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-11 15:17:45 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -7,16 +7,9 @@ class Mycog:
     def __init__(self, bot):
         self.bot = bot
         
-@commands.command(pass_context=True)
-    async def excom(self, ctx):
-        """CTX example command"""
-        author = ctx.message.author
-        description = ("Short little description with a link to "
-                       "the [guide](https://github.com/Redjumpman/Jumper-Cogs/wiki/Discord-Coding-Guide)")
-        field_name = "Generic Name"
-        field_contents = "Example contents for this field"
-        footer_text = "Hi. I am a footer text. I look small when displayed."
-
+@client.event
+async def on_message(message):
+    if message.content.startswith('about'):
         embed = discord.Embed(colour=0xFF0000, description=description)  # Can use discord.Colour()
         embed.title = "Cool title for my embed"
         embed.set_author(name=str(author.name), icon_url=author.avatar_url)
