efd8b3048340b89d0d5bc18c23b2530f6c8425e3
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-11 15:23:39 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -7,9 +7,15 @@ class Mycog:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def about(self):
-        """This does stuff!"""
+@commands.command(pass_context=True)
+    async def about(self, ctx)
+            """CTX example command"""
+        author = ctx.message.author
+        description = ("Short little description with a link to "
+                       "the [guide](https://github.com/Redjumpman/Jumper-Cogs/wiki/Discord-Coding-Guide)")
+        field_name = "Generic Name"
+        field_contents = "Example contents for this field"
+        footer_text = "Hi. I am a footer text. I look small when displayed."
 
 embed=discord.Embed(title="basic test", url='http://www.mydomain.com', description="This is a description", color=0x207cee)
 embed.set_author(name="UnseenMagik", url='http://www.discordlink.com', icon_url='http://imageicon.com')
