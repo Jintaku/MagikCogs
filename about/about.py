fe276179f0d6d7d74dfd43e7f22f129c09645554
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-12 07:52:36 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -7,21 +7,35 @@ class about:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def about(self):
-        """This does stuff!"""
+const embed = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
-        #Your code will go here
-        
-embed=discord.Embed(title="basic test", url='http://www.mydomain.com', description="This is a description", color=0x207cee)
-embed.set_author(name="UnseenMagik", url='http://www.discordlink.com', icon_url='http://imageicon.com')
-embed.set_thumbnail(url='http://www.imageurl.com')
-embed.add_field(name="Test Field 1", value="Result", inline=True)
-embed.add_field(name="Test Field 2", value="Inline text", inline=True)
-embed.set_footer(text="Footer notes here. ")
-
-
-await self.bot.say(embed=embed)
+  message.channel.send({embed});
         
 
 
