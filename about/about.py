b390d7ce381ee7d1d2d056be9bf71adfe6ccc673
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-12 11:35:04 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -11,11 +11,11 @@ class Mycog:
     async def about(self):
         """About Magik Bot"""
         
-        embed=discord.Embed(title="About Magik Bot", url='http://www.magikbot.co.uk', description="A discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
+        embed=discord.Embed(title="About Magik Bot", url='http://www.magikbot.co.uk', description="A discord bot made with love. Created for Discord Administration support. <br>With hundreds of commands, admin, mod, support, games, fun and more.<br> For more information see below:<p>", color=0x207cee)
         embed.set_author(name="Magik bot", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
-        embed.add_field(name="Discord Support", value= url="https://discord.gg/kQjTw5Z", description="Visit our Discord Server", inline=True)
-        embed.add_field(name="Website", value= url="http://www.magikbot.co.uk", description="Visit our Website", inline=True)
+        embed.add_field(name="Discord Support", value="https://discord.gg/kQjTw5Z", inline=True)
+        embed.add_field(name="Website", value="http://www.magikbot.co.uk", inline=True)
         embed.add_field(name="Auther", value="UnseenMagik", inline=False)
         embed.add_field(name="Version", value="API version 0.16.11", inline=True)
         embed.set_footer(text="Magik Bot - Providing Discord support since September 2017")
