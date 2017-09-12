106f9ef87d6cd0e7e760cd80ae4aa11a6a17faa6
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-12 08:44:12 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -1,37 +1,27 @@
 import discord
 from discord.ext import commands
 
-const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+class Mycog:
+    """My custom cog that does stuff!"""
 
-  message.channel.send({embed});
+@commands.command(pass_context=True)
+    async def excom(self, ctx):
+        """CTX example command"""
+        author = ctx.message.author
+        description = ("Short little description with a link to "
+                       "the [guide](https://github.com/Redjumpman/Jumper-Cogs/wiki/Discord-Coding-Guide)")
+        field_name = "Generic Name"
+        field_contents = "Example contents for this field"
+        footer_text = "Hi. I am a footer text. I look small when displayed."
 
+        embed = discord.Embed(colour=0xFF0000, description=description)  # Can use discord.Colour()
+        embed.title = "Cool title for my embed"
+        embed.set_author(name=str(author.name), icon_url=author.avatar_url)
+        embed.add_field(name=field_name, value=field_contents)  # Can add multiple fields.
+        embed.set_footer(text=footer_text)
 
-
+        await self.bot.say(embed=embed)
+        
+        
 def setup(bot):
-    bot.add_cog(about(bot))
+    bot.add_cog(Mycog(bot))
