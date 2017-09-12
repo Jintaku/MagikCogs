b10e902bb61888d590b2e9264202baedc92c8f08
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-12 07:49:39 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -1,29 +1,29 @@
 import discord
 from discord.ext import commands
 
-class About:
-    """This is information about Magik Bot"""
-    
-        def __init__(self, bot):
+class about:
+    """Magik Bot About!"""
+
+    def __init__(self, bot):
         self.bot = bot
 
-@commands.command(pass_context=True)
-    async def excom(self, ctx):
-        """CTX example command"""
-        author = ctx.message.author
-        description = ("Short little description with a link to "
-                       "the [guide](https://github.com/Redjumpman/Jumper-Cogs/wiki/Discord-Coding-Guide)")
-        field_name = "Generic Name"
-        field_contents = "Example contents for this field"
-        footer_text = "Hi. I am a footer text. I look small when displayed."
+    @commands.command()
+    async def about(self):
+        """This does stuff!"""
 
-        embed = discord.Embed(colour=0xFF0000, description=description)  # Can use discord.Colour()
-        embed.title = "Cool title for my embed"
-        embed.set_author(name=str(author.name), icon_url=author.avatar_url)
-        embed.add_field(name=field_name, value=field_contents)  # Can add multiple fields.
-        embed.set_footer(text=footer_text)
+        #Your code will go here
         
-        await self.bot.say(embed=embed)
+embed=discord.Embed(title="basic test", url='http://www.mydomain.com', description="This is a description", color=0x207cee)
+embed.set_author(name="UnseenMagik", url='http://www.discordlink.com', icon_url='http://imageicon.com')
+embed.set_thumbnail(url='http://www.imageurl.com')
+embed.add_field(name="Test Field 1", value="Result", inline=True)
+embed.add_field(name="Test Field 2", value="Inline text", inline=True)
+embed.set_footer(text="Footer notes here. ")
+
+
+await self.bot.say(embed=embed)
+        
+
 
 def setup(bot):
-    bot.add_cog(About(bot))
+    bot.add_cog(about(bot))
