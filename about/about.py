e98813d02bdc3ec59004f65d1d9d60c99648855f
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-12 08:35:09 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -7,15 +7,36 @@ class about:
     def __init__(self, bot):
         self.bot = bot
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('hello'):
-        embed = discord.Embed(title="Tile", description="Desc", color=0x00ff00)
-        embed.add_field(name="Fiel1", value="hi", inline=False)
-        embed.add_field(name="Field2", value="hi2", inline=False)
-        
-        await client.send_message(message.channel, embed=embed)
-        
+const embed = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+  message.channel.send({embed});
+
 
 
 def setup(bot):
