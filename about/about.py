af574e77f42e9ad56f25a8e96f845a23efa8a90f
unseenmagik <30931287+unseenmagik@users.noreply.github.com>
2017-09-12 11:00:38 +0100

    Update about.py

--- about/about.py
+++ about/about.py
@@ -8,17 +8,17 @@ class Mycog:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def excom(self, ctx):
-        """CTX example command"""
+    async def about(self, ctx):
+        """About Magik Bot"""
         author = ctx.message.author
         description = ("Short little description with a link to "
                        "the [guide](https://github.com/Redjumpman/Jumper-Cogs/wiki/Discord-Coding-Guide)")
-        field_name = "Generic Name"
-        field_contents = "Example contents for this field"
-        footer_text = "Hi. I am a footer text. I look small when displayed."
+        field_name = "More Support at:"
+        field_contents = "This will be replaced with a URL"
+        footer_text = "Magik Bot created in Discord.py by UnseenMagik since 2017."
 
-        embed = discord.Embed(colour=0xFF0000, description=description)  # Can use discord.Colour()
-        embed.title = "Cool title for my embed"
+        embed = discord.Embed(colour=0x0000FF, description=description)  # Can use discord.Colour()
+        embed.title = "About Magik Bot"
         embed.set_author(name=str(author.name), icon_url=author.avatar_url)
         embed.add_field(name=field_name, value=field_contents)  # Can add multiple fields.
         embed.set_footer(text=footer_text)
