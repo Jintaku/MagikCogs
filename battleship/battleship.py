fb2ece427dd3fa4407275a988d6b129b875f18e3
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2017-10-03 14:14:03 +0100

    Update battleship.py
    changed guide

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -34,14 +34,15 @@ X = Miss
 S = Hit
 M - Location of ships (shown at the end of the game)
 
- X - A X I S
+X - A X I S
 
-5 O O O O O   Y
-4 O O O O O   -
-3 O O O O O   A
-2 O O O O O   X
-1 O O O O O   I
-0 1 2 3 4 5   S```"""
+5   O O O O O   Y
+4   O O O O O   -
+3   O O O O O   A
+2   O O O O O   X
+1   O O O O O   I
+0               S
+  0 1 2 3 4 5```"""
 
         """
              1 2 3 4 5 6 7 8 9 10
