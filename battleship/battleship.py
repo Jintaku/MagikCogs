c37a3df20d886636205f73cadea8e23a8914faf2
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2017-10-04 12:34:51 +0100

    Update battleship.py

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -7,6 +7,21 @@ class Battleship:
 
     def __init__(self, bot):
         self.bot = bot
+        
+        @commands.command(pass_context=True)
+    async def battleshiphelp(self):
+        """About Magik Bot"""
+        
+        embed=discord.Embed(title="About Magik Bot", url='http://www.magikbot.co.uk', description="A discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
+        embed.set_author(name="Magik bot", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
+        embed.add_field(name="Discord Support", value="https://discord.gg/kQjTw5Z", inline=True)
+        embed.add_field(name="Website", value="http://www.magikbot.co.uk", inline=True)
+        embed.add_field(name="Auther", value="UnseenMagik", inline=False)
+        embed.add_field(name="Version", value="API version 0.16.11", inline=True)
+        embed.set_footer(text="Magik Bot - Providing Discord support since September 2017")
+        
+        await self.bot.say(embed=embed)
 
 
 
@@ -26,18 +41,7 @@ class Battleship:
         channel = ctx.message.channel
 
 
-        guide = """
-        
-        embed=discord.Embed(title="About Magik Bot", url='http://www.magikbot.co.uk', description="A discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
-        embed.set_author(name="Magik bot", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
-        embed.add_field(name="Discord Support", value="https://discord.gg/kQjTw5Z", inline=True)
-        embed.add_field(name="Website", value="http://www.magikbot.co.uk", inline=True)
-        embed.add_field(name="Auther", value="UnseenMagik", inline=False)
-        embed.add_field(name="Version", value="API version 0.16.11", inline=True)
-        embed.set_footer(text="Magik Bot - Providing Discord support since September 2017")
-        
-        await self.bot.say(embed=embed)
+        guide = """ 
         ```To play Battleship, you must guess the location of my ships. To make your guess, enter your X & Y position in one command sepperated by a space. An example would be 3 2
 
 O = Open Target
