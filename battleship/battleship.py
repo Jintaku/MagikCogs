0f480ddf91d6e344d2c0029cb52d1ef316dac516
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2017-10-04 12:44:35 +0100

    Update battleship.py
    added embed to guide.

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -25,25 +25,19 @@ class Battleship:
         author = ctx.message.author
         channel = ctx.message.channel
 
-        embed=discord.Embed(title="About Magik Bot", url='http://www.magikbot.co.uk', description="A discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
-        embed.set_author(name="Magik bot", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
+        embed=discord.Embed(title="About Battleship", description="A simple game of Battle Ships built into Magik Bot. o - Open Target, X - Missed Target, S = Target Hit M = Location of ships at the end of the game", color=0x207cee)
+        embed.set_author(name="Magik bot", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/attachments/355249562719617024/365100412874784768/Battleship-ubicom-VIDEO-launch_trailer_2016_08_02-712x712_Desktop_261122.png')
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
-        embed.add_field(name="Discord Support", value="https://discord.gg/kQjTw5Z", inline=True)
-        embed.add_field(name="Website", value="http://www.magikbot.co.uk", inline=True)
-        embed.add_field(name="Auther", value="UnseenMagik", inline=False)
-        embed.add_field(name="Version", value="API version 0.16.11", inline=True)
+        embed.add_field(name="How to play", value="Enter your X and Y value as a comment like this `4 2`", inline=True)
+        embed.add_field(name="How many turns", value="You have 9 attempts to hit my 4 ships", inline=True)
+        embed.add_field(name="Auther", value="UnseenMagik & Potato", inline=False)
+        embed.add_field(name="Version", value="Version 1.0.6", inline=True)
         embed.set_footer(text="Magik Bot - Providing Discord support since September 2017")
         
         await self.bot.say(embed=embed)
 
         guide = """ 
-        ```To play Battleship, you must guess the location of my ships. To make your guess, enter your X & Y position in one command sepperated by a space. An example would be 3 2
-
-O = Open Target
-X = Miss
-S = Hit
-M - Location of ships (shown at the end of the game)
-
+        ```
   ┌───┬───┬───┬───┬───┬───┐
 Y │ 5 │ o │ o │ o │ o │ o │
   ├───┼───┼───┼───┼───┼───┤
