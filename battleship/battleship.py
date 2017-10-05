d8c47ad262aadb2584aa21f740ba5efd509df5d8
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2017-10-05 08:06:03 +0100

    Update battleship.py

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -91,7 +91,7 @@ O O O O O 5 I
         """
 
         for x in range(5): #Size of the board
-            board.append("[:thumbsup:]" * 5)
+            board.append(":thumbsup:" * 5)
 
         def print_board(board): #Making the board
             i = "```\n"
