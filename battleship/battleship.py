ed9b603ab6e8bb0775d18b1898c5a09b8f68c592
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2017-10-06 07:45:01 +0100

    Update battleship.py
    adjusted lineup on y axis

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -47,12 +47,12 @@ class Battleship:
         embed.add_field(
             name="Battleship Board Layout",
             value=("```Y   1 O O O O O\n"
-                   "       2 O O O O O\n"
-                   "   A   3 O O O O O\n"
-                   "   X   4 O O O O O\n"
-                   "   I   5 O O O O O\n"
-                   "   S   0 1 2 3 4 5\n"
-                   "       X  A X I S```"), inline=True)
+                   "    2 O O O O O\n"
+                   "A   3 O O O O O\n"
+                   "X   4 O O O O O\n"
+                   "I   5 O O O O O\n"
+                   "S   0 1 2 3 4 5\n"
+                   "    X   A X I S```"), inline=True)
         embed.set_footer(
             text="Magik Bot - Providing Discord support since September 2017")
         
