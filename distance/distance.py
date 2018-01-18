8fd737476f321b0dd67709d0c6453ca8eb29b72c
xDp64xG <brow.paul64@gmail.com>
2018-01-18 08:42:48 -0500

    Update distance.py

--- distance/distance.py
+++ distance/distance.py
@@ -25,7 +25,7 @@ class Distance:
         def error_embed_2(self):
             embed=discord.Embed(
                 title="Error:",
-                description="Your message is invalid. Please use this format\n``!distance <num1> <num2> <num3> <num4>\nWhich would look like this: ``!distance 51.301597 -0.598019 51.270664 -0.594132``",
+                description="Your message is invalid. Please use this format\n``!distance <num1> <num2> <num3> <num4>``\nWhich would look like this: ``!distance 51.301597 -0.598019 51.270664 -0.594132``",
                 color=0x207cee)
             return embed
 
