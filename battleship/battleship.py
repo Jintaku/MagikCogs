b6d3c6e4cd40792774f1dc820fe221230ecb8918
xDp64xG <brow.paul64@gmail.com>
2018-02-02 12:23:54 -0500

    Update battleship.py

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -6,40 +6,7 @@ class Battleship:
 
     def __init__(self, bot):
         self.bot = bot
-    """@commands.command()
-    async def battlehelp(self):
-
-        embed=discord.Embed(
-            title="About Battleship", 
-            description="~ A simple game of Battle Ships.\n:black_circle: = Open Target\n:red_circle: = Missed Target\n:large_blue_circle: = Target Hit\n⚪ = Location of ships (at the end of the game)", 
-            color=0x207cee)
-        embed.set_author(
-            name="Magik Bot",
-            url = 'http://www.magikbot.co.uk',
-            icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
-        embed.set_thumbnail(
-            url='https://cdn.discordapp.com/attachments/355249562719617024/365100412874784768/Battleship-ubicom-VIDEO-launch_trailer_2016_08_02-712x712_Desktop_261122.png')
-        embed.add_field(
-            name="How to play", 
-            value="Enter your X and Y value as a comment like this `4 2`. No prefix required. Type `cancel` to stop the game.", 
-            inline=True)
-        embed.add_field(
-            name="How many turns", 
-            value="You have 10 attempts to hit my 3 ships(2 ships that are 2 by 1, 1 ship that is 1 by 1)", 
-            inline=True)
-        embed.add_field(
-            name="Battleship Board Layout",
-            value=(:regional_indicator_y: :one: :zero: :zero: :zero: :zero: :zero:
-<:gap:367578219316248577> :two: :zero: :zero: :zero: :zero: :zero:
-:regional_indicator_a: :three: :zero: :zero: :zero: :zero: :zero:
-:regional_indicator_x: :four: :zero: :zero: :zero: :zero: :zero:
-:regional_indicator_i: :five: :zero: :zero: :zero: :zero: :zero:
-:regional_indicator_s: :zero: :one: :two: :three: :four: :five:
-<:gap:367578219316248577> :regional_indicator_x: <:gap:367578219316248577> :regional_indicator_a: :regional_indicator_x: :regional_indicator_i: :regional_indicator_s:), inline=True)
-        embed.set_footer(
-            text="Possible by - xDp64x and UnseenMagik")
-        
-        instructions = await self.bot.say(embed=embed)""" 
+ 
     @commands.command(pass_context=True)
     async def battleship(self, ctx):
         
